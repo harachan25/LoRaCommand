9da28c7a38059ff3dcaa1768e5e49b53119733a4
harachan25 <mika@outlook.jp>
2022-07-03 01:45:58 +0900

    error: how to change bitDouble

--- loraCommand.py
+++ loraCommand.py
@@ -3,6 +3,7 @@
 
 import binascii
 import datetime
+import struct
 
 
 #4040ff4154 + <len({22+文字数}/2)> + AAAA0001ffffffffffff + <文字数> + <“[done*180000]*10”>          +  AA
@@ -34,13 +35,24 @@ if c0 == "pos":
     c2 = input()
     i = 0
     j = 0
-    C1 = ""
-    C2 = ""
+    C1 = 0 #-3.141519
+    C1_1 = struct.pack('>d', C1) #b'\xc0\t!\xd4\xb6\xa6\x19\xdb'
+    C1_2 = struct.unpack('>Q', C1_1)[0] #13837628527553681883
+    C1_3 = bin(C1_2) #'0b1100000000001001001000011101010010110110101001100001100111011011'
+    C1_3[2:len(C1_3)] #'1100000000001001001000011101010010110110101001100001100111011011'
+    C2 = 0
+    C2_1 = struct.pack('>d', C2)
+    C2_2 = struct.unpack('>Q', C2_1)[0]
+    C2_3 = bin(C2_2)
+    C2_3[2:len(C2_3)]
     for i in range(8): #8回繰り返す
-        c1_i = c1[8*i:8*i+8] #文字列c1から8文字(8桁)ずつ取り出す
-        c2_i = c2[8*i:8*i+8]
-        C1 = C1 + str(binascii.hexlify(c1_i.encode())) #取り出した8文字を16進数に変換して足す
-        C2 = C2 + str(binascii.hexlify(c2_i.encode()))
+        c1_i = int(c1[8*i:8*i+8]) #文字列c1から8文字(8桁)ずつ取り出す
+        c2_i = int(c2[8*i:8*i+8])
+        print(hex((c1_i)))
+        print(hex((c2_i)))
+        C1 = str(C1) + str(str(c1_i)[2:len(hex(c1_i))-1]) #取り出した8文字を16進数に変換して足す
+        C2 = str(C2) + str(str(c2_i)[2:len(hex(c2_i))-1])
+        print(C1)
         i = i + 1
     c9 = c0 + "*" + C1 + "*" + C2 + "*" + time
 elif c0 == "rec": #受信したことを伝える
@@ -75,6 +87,18 @@ elif c0 == "err":
     
 elif c0 == "remi":
     c9 = c0
+elif c0 == "sacc":
+    c1 = input() #データ数(以下データ数分だけ繰り返す): int
+    c2 = input() #緯度
+    c3 = input() #経度
+    c4 = input() #加速度L2ノルム: bitDouble
+    c9 = c0 + "*" + str(c1) + "*" + c2 + "*" + c3 + "*" + c4
+elif c0 == "srun":
+    c1 = input() #状態(反転/0,横転/1,復帰不可/2): int
+    c2 = input() #緯度
+    c3 = input() #経度
+    c9 = c0 + "*" + str(c1) + "*" + c2 + "*" + c3
+    
 else:
   c9= "none"
   
